a5bd48b301f70a63c544b5094e6b30d8f538fb72
Stefanos Ugbit <stefanosu422@gmail.com>
2019-05-06 23:37:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,7 +28,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, element)
-  array.insert(element,3)
+def using_insert(array, new_element)
+  array.insert(new_element,3)
 end
 
